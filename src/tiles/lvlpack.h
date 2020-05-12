417b460932f492becb55eb0aca26bdd32a47ea4a
commandblockguy <commandblockguy1@gmail.com>
2020-05-12 16:17:47 -0500

    Fix block heights for some levels

--- src/tiles/lvlpack.h
+++ src/tiles/lvlpack.h
@@ -4,25 +4,25 @@
 #define lvlpack_header
 #include <stdint.h>
 
-extern uint8_t lvl7_compressed[22];
-extern uint8_t lvl5_compressed[22];
-extern uint8_t lvl18_compressed[43];
-extern uint8_t lvl16_compressed[41];
 extern uint8_t lvl3_compressed[34];
-extern uint8_t lvl6_compressed[33];
-extern uint8_t lvl19_compressed[26];
-extern uint8_t lvl17_compressed[48];
-extern uint8_t lvl15_compressed[35];
-extern uint8_t lvl8_compressed[47];
-extern uint8_t lvl11_compressed[49];
+extern uint8_t lvl5_compressed[22];
 extern uint8_t lvl12_compressed[41];
+extern uint8_t lvl1_compressed[25];
 extern uint8_t lvl20_compressed[7];
-extern uint8_t lvl14_compressed[43];
 extern uint8_t lvl10_compressed[36];
-extern uint8_t lvl1_compressed[29];
+extern uint8_t lvl16_compressed[41];
+extern uint8_t lvl11_compressed[49];
+extern uint8_t lvl6_compressed[33];
+extern uint8_t lvl17_compressed[48];
+extern uint8_t lvl7_compressed[22];
+extern uint8_t lvl18_compressed[43];
 extern uint8_t lvl4_compressed[37];
+extern uint8_t lvl8_compressed[47];
 extern uint8_t lvl9_compressed[43];
-extern uint8_t lvl13_compressed[26];
 extern uint8_t lvl2_compressed[32];
+extern uint8_t lvl19_compressed[26];
+extern uint8_t lvl15_compressed[35];
+extern uint8_t lvl13_compressed[26];
+extern uint8_t lvl14_compressed[43];
 
 #endif
